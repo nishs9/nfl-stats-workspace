4fb7754707bd574d9e96ec9e0787ba77e5f6e6f5
Nishant Sethunath <snishk@gmail.com>
2025-01-13 22:04:06 -0800

    set up a script to get data from nflverse and set up a sqlite db to store it

---
+++ nfl_data_retrieval.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+import constants
+import sqlite3
+
+# Returns full play-by-play data for a given season
+# Season should be passed in as an integer
+# Includes regular and postseason games
+def get_league_play_by_play_data(season):
+    base_url = 'https://github.com/nflverse/nflverse-data/releases/download/pbp/play_by_play_' + str(season) + '.csv.gz'
+    pbp_data = pd.read_csv(base_url, compression='gzip', low_memory=False)
+    output_file = f"raw_pbp_data/{season}_NFL.csv"
+    pbp_data.to_csv(output_file, index=False)
+    return pbp_data
+
+# Returns offensive stats for all players in a given season 
+# Season should be passed in as an integer
+# Includes regular, postseason, and reg+post combined stats
+def get_offensive_player_stats(season):
+    base_url = 'https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_season_' + str(season) + '.csv'
+    player_stats = pd.read_csv(base_url, low_memory=False)
+    output_file = f"raw_player_stats/{season}_NFL.csv"
+    player_stats.to_csv(output_file, index=False)
+    return player_stats
+
+if __name__ == "__main__":
+    try:
+        db_conn = sqlite3.connect("nfl_stats.db")
+        #get_league_play_by_play_data(2024)
+        player_stats_season_df = get_offensive_player_stats(2024)
+        player_stats_season_df.to_sql('player_stats_season_2024', db_conn, if_exists='replace', index=False)
+    finally:
+        db_conn.close()
